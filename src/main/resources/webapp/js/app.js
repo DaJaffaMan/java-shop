9ff0b10ac67edae90ea363b6cdae89ef8ef41364
jackjefferies <jack-jefferies@live.co.uk>
2015-12-20 00:47:57 +0000

    commiting latest angular design

--- src/main/resources/webapp/js/app.js
+++ src/main/resources/webapp/js/app.js
@@ -1,1 +1,23 @@
-var app = angular.module("app", []);
+var app = angular.module('productApp', []);
+
+app.controller('productController', function($scope, product) {
+$scope.getProduct = function(){
+product.getProductDetails();
+}
+//$scope.addProduct = function(productName, stock, price) {
+//product.addProductDetails();
+//}
+});
+
+app.service('product', function () {
+
+    return {
+
+getProductDetails: function($scope, $http) {
+                               $http.get('http://localhost:4567/get/product/' + document.getElementById("product")).
+                                   success(function(data) {
+                                       $scope.product = data;
+                                   });
+                               }
+    };
+});
--- src/main/resources/webapp/js/controller.js
+++
@@ -1,5 +0,0 @@
-<script src="/js/Product.js"></script>
-
-app.controller("appController", function($scope) {
-    $scope.products = getProduct();
-});
