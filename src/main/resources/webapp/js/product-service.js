80013f42a05a8bc23076ee7d78402a2b8f98f04d
jackjefferies <jack-jefferies@live.co.uk>
2016-01-01 18:22:33 +0000

    updating product service to get host url instead of being hardcoded

--- src/main/resources/webapp/js/product-service.js
+++ src/main/resources/webapp/js/product-service.js
@@ -1,11 +1,11 @@
 angular.module('productApp').service('productService', ['$http', function($http) {
     return {
         getProductDetails: function(product) {
-            return $http.get('http://localhost:4567/get/product/' + product);
+            return $http.get(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/get/product/' + product);
         },
 
         addProductDetails: function(product_name, product_stock, product_price) {
-                    $http.post('http://localhost:4567/add/' + product_name + '/' + product_stock + '/' + product_price);
+                    $http.post(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/add/' + product_name + '/' + product_stock + '/' + product_price);
         }
     };
 }]);
